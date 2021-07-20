7119f072f4a108768b0f690b2d741f31f49e73f3
Stephen Nguyen <snguy3nn@gmail.com>
2021-07-20 15:01:50 -0500

    Initial commit

---
+++ superheroesdatabase/superheroesdatabase/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for superheroesdatabase project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superheroesdatabase.settings')
+
+application = get_asgi_application()
